d290636840f4878bc43f1d380aa8c4bfc2150f85
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2021-10-27 19:21:55 -0500

    massotter, don't show aliases in help

--- commands/help.ts
+++ commands/help.ts
@@ -2,11 +2,18 @@ import { Context } from "../util";
 import * as commands from "../commands";
 
 export default async function ({ message, api }: Context) {
+  let content = "👌 You can ||(or can't idk)|| use:\n";
+
+  let unique = new Set();
+  for (const [command, run] of Object.entries(commands)) {
+    const id = run.toString();
+    if (!unique.has(id)) {
+      unique.add(id);
+      content += "\n`" + command + "`";
+    }
+  }
+
   api.createMessage(message.channel_id, {
-    content:
-      "👌 You can ||(or can't idk)|| use: " +
-      Object.keys(commands)
-        .map(command => "`" + command + "`")
-        .join(", ")
+    content
   });
 }
--- commands/otter.ts
+++ commands/otter.ts
@@ -1,11 +1,23 @@
 import { Context } from "../util";
 import * as robert from "robert";
 
-export default async function (ctx: Context) {
-  const otter = await robert.get("https://otter.bruhmomentlol.repl.co/random").full().send();
-  ctx.api.createMessage(
-    ctx.message.channel_id,
-    {},
-    { name: "otter." + otter.headers["x-file-ext"], value: otter.data }
-  );
+export default async function ({ message, args, api }: Context) {
+  const files = [];
+  let amount = 1;
+  if (args[0]) {
+    amount = Math.round(parseInt(args[0]));
+    if (!amount) return api.createMessage(message.channel_id, { content: "Invalid amount" });
+    if (amount < 1)
+      return api.createMessage(message.channel_id, { content: "Amount under minimum" });
+
+    if (amount > 10)
+      return api.createMessage(message.channel_id, { content: "Amount over maximum" });
+  }
+
+  for (let i = 0; i < amount; i++) {
+    const otter = await robert.get("https://otter.bruhmomentlol.repl.co/random").send();
+    files.push({ name: "otter" + i + "." + otter.headers["x-file-ext"], value: otter });
+  }
+
+  api.createMessage(message.channel_id, { content: "🦦" }, files);
 }
