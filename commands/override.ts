a04a0544ae3c2688062461d579452b86ca5f0e56
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2021-12-01 23:24:14 -0600

    add fire support

--- commands/override.ts
+++ commands/override.ts
@@ -1,13 +1,50 @@
 import { Context } from "../util";
 
+interface DiscordOverride {
+  targetBuildOverride: Record<string, object>;
+  releaseChannel: string;
+  validForUserIds: string[];
+  allowLoggedOut: boolean;
+  expiresAt: string;
+}
+
+interface FireOverride {
+  bucket: number;
+  experiment: number;
+  id: number;
+  releaseChannel: string;
+  validForUserIds: string[];
+}
+
 export default function ({ message, args, api }: Context) {
+  if (!args.length)
+    return api.createMessage(message.channel_id, { content: "Please specify an override URL" });
+
+  let url: URL;
   try {
-    const data = JSON.parse(
-      Buffer.from(
-        decodeURIComponent(args.join("").split("?s=")[1].split(".")[1]),
-        "base64"
-      ).toString()
-    );
+    url = new URL(args.join(" "));
+  } catch {
+    return api.createMessage(message.channel_id, { content: "Invalid URL" });
+  }
+
+  const param = url.searchParams.get("s");
+  if (url.host === "discord.com" || url.host === "discordapp.com") {
+    const [, encoded] = param.split(".");
+    if (!encoded) return api.createMessage(message.channel_id, { content: "Invalid Parameter" });
+
+    let decoded: string;
+    try {
+      decoded = Buffer.from(encoded, "base64").toString();
+    } catch {
+      return api.createMessage(message.channel_id, { content: "Invalid Base64" });
+    }
+
+    let data: DiscordOverride;
+    try {
+      data = JSON.parse(decoded);
+    } catch {
+      return api.createMessage(message.channel_id, { content: "Invalid JSON" });
+    }
 
     const fields = Object.entries(data.targetBuildOverride).map(([platform, target]) => ({
       name: platform,
@@ -36,19 +73,60 @@ export default function ({ message, args, api }: Context) {
             },
             {
               name: "Allow Logged Out",
-              value: data.allowLoggedOut.toString(),
+              value: data.allowLoggedOut ? "Yes" : "No",
               inline: true
             },
-
             ...fields
           ]
         }
       ]
     });
-  } catch (e) {
+  } else if (url.host === "inv.wtf") {
+    let decoded: string;
+    try {
+      decoded = Buffer.from(param, "base64").toString();
+    } catch {
+      return api.createMessage(message.channel_id, { content: "Invalid Base64" });
+    }
+
+    let data: FireOverride;
+    try {
+      data = JSON.parse(decoded);
+    } catch {
+      return api.createMessage(message.channel_id, { content: "Invalid JSON" });
+    }
+
     api.createMessage(message.channel_id, {
-      content: "⚠ " + e.message,
-      allowedMentions: { parse: [] }
+      content: "```json\n" + JSON.stringify(data, null, 2) + "```",
+      embeds: [
+        {
+          fields: [
+            {
+              name: "ID",
+              value: data.id.toString(),
+              inline: true
+            },
+            {
+              name: "Bucket",
+              value: data.bucket.toString(),
+              inline: true
+            },
+            {
+              name: "Experiment",
+              value: data.experiment.toString(),
+              inline: true
+            },
+            {
+              name: "Release Channel",
+              value: data.releaseChannel ?? "None"
+            },
+            {
+              name: "Users",
+              value: data.validForUserIds.join("\n") || "None"
+            }
+          ]
+        }
+      ]
     });
-  }
+  } else return api.createMessage(message.channel_id, { content: "Invalid origin" });
 }
