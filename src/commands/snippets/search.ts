e33122c5defffe1402a80108b47a27bc8e392ab5
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2022-02-09 12:59:40 -0600

    this is stupid but lol

--- src/commands/snippets/search.ts
+++ src/commands/snippets/search.ts
@@ -22,13 +22,34 @@ export default async function ({ message, args, api }: Context) {
     return api.createMessage(message.channel_id, { content: "No results found" });
 
   const { item } = results.shift();
+
+  let avatar: string;
+  if (item.author) {
+    const [member] = await api.searchGuildMembers(message.guild_id, {
+      query: item.author.replaceAll("@", "").replaceAll("#", "").replaceAll("```", ""),
+      limit: "1"
+    });
+
+    if (member) {
+      avatar = member.avatar
+        ? `https://cdn.discordapp.com/guilds/${message.guild_id}/users/${member.user.id}/avatars/${
+            member.user
+          }.${member.avatar.startsWith("a_") ? "gif" : "png"}`
+        : member.user.avatar
+        ? `https://cdn.discordapp.com/avatars/${member.user.id}/${member.user.avatar}.${
+            member.user.avatar.startsWith("a_") ? "gif" : "png"
+          }`
+        : `https://cdn.discordapp.com/embed/avatars/${member.user.discriminator % 5}.png`;
+    }
+  }
+
   await api.createMessage(
     message.channel_id,
     {
       embeds: [
         {
           title: item.title,
-          author: { name: item.author },
+          author: item.author && { name: item.author, icon_url: avatar },
           description: item.description,
           fields: results.length && [
             {
