566226397b0ac755cb8a8c80b895f409e231dcb4
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2022-02-09 11:59:42 -0600

    add quick run

--- src/commands/snippets/export.ts
+++ src/commands/snippets/export.ts
@@ -3,12 +3,20 @@ import { createReadStream } from "fs";
 import { Context, exec } from "../../util";
 import { resolve } from "path";
 
-export const name = "snippets";
-export const aliases = ["snippets export", "snippets build"];
+export const name = "snippets export";
+export const aliases = ["snippets code", "snippets build"];
 
 export default async function ({ message, api }: Context) {
   await exec("git pull", { cwd: BASE_DIR });
 
   const snippets = createReadStream(resolve(BASE_DIR, "build", "output", "snippets.js"));
-  await api.createMessage(message.channel_id, {}, { name: "snippets.js", value: snippets });
+  const { id, attachments } = await api.createMessage(
+    message.channel_id,
+    {},
+    { name: "snippets.js", value: snippets }
+  );
+  const [{ url }] = attachments;
+  api.editMessage(message.channel_id, id, {
+    content: "```js\neval(await (await fetch(" + JSON.stringify(url) + ")).text())```"
+  });
 }
