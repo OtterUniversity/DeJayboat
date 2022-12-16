6058644cfc41b6b7a213c7f643f142c4109ce91f
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2022-12-16 15:25:41 -0600

    make promise angry

--- src/commands/admin/eval.ts
+++ src/commands/admin/eval.ts
@@ -17,7 +17,7 @@ export default async function ({ message, args, api, ws }: Context) {
     let out = eval(args.join(" "));
     if (out instanceof Promise) {
       api.createMessage(message.channel_id, {
-        content: "<a:crumbdance:877043850890317855> Resolving Promise"
+        content: "<a:crumbdance:877043850890317855> Resolving <@110090225929191424>"
       });
 
       try {
