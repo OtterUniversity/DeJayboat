29f542716d3baade71326f674674a892f520d50c
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2023-11-15 13:45:00 -0600

    simplify cache command

--- src/commands/cache.ts
+++ src/commands/cache.ts
@@ -8,28 +8,38 @@ export default async function ({ message, args, api }: Context) {
   const userId = args[0];
   if (!userId || !/^\d+$/.test(userId)) return api.createMessage(message.channel_id, { content: "Invalid user" });
 
-  try {
-    await api.getUser(userId);
-  } catch {
+  const isValidUser = await api
+    .getUser(userId)
+    .then(() => true)
+    .catch(() => false);
+
+  if (!isValidUser) {
     await api.createMessage(message.channel_id, { content: "kys" });
     return;
   }
 
-  try {
-    await api.getGuildMember(message.guild_id, userId);
+  const isInServer = await api
+    .getGuildMember(message.guild_id, userId)
+    .then(() => true)
+    .catch(() => false);
+  if (!isInServer) {
     await api.createMessage(message.channel_id, { content: "User is in the server" });
     return;
-  } catch {}
+  }
 
-  try {
-    await api.getGuildBan(message.guild_id, userId);
-    // if they're already banned, ban and unban
+  const { isBanned, ban } = await api
+    .getGuildBan(message.guild_id, userId)
+    .then(ban => ({ isBanned: true, ban }))
+    .catch(() => ({ isBanned: false, ban: null }));
+
+  if (isBanned) {
+    // if they're already banned, unban and ban
     await api.removeGuildBan(message.guild_id, userId);
     await api.createGuildBan(message.guild_id, userId, {
       deleteMessageDays: 0,
-      reason: ".cache by " + message.author.username + "#" + message.author.discriminator
+      reason: ban.reason
     });
-  } catch {
+  } else {
     // if they arent banned, ban and unban
     await api.createGuildBan(message.guild_id, userId, {
       deleteMessageDays: 0,
