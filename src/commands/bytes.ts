a155477c4c9841df0461134e4adf0992ad8b3fa5
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2025-09-06 23:18:46 -0500

    fuck esm incompatibility

--- src/commands/bytes.ts
+++ src/commands/bytes.ts
@@ -1,9 +1,10 @@
-import prettyBytes from "pretty-bytes";
+const prettyBytesPkg = import("pretty-bytes");
 import { Context } from "../util";
 
 export const open = true;
 export const name = "bytes";
 export default async function ({ message, args, api }: Context) {
+  const prettyBytes = (await prettyBytesPkg).default;
   let bytes = parseInt(args[0]);
   if (!bytes) return await api.createMessage(message.channel_id, { content: "invalid amount of bytes" });
 
