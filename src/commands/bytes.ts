c58dc9aa9947e79d2b39b8cb925210ff475bdfde
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2025-09-06 23:24:29 -0500

    this is so stupid i dont care anymore

--- src/commands/bytes.ts
+++ src/commands/bytes.ts
@@ -1,10 +1,9 @@
-const prettyBytesPkg = import("pretty-bytes");
+import prettyBytes from "../pretty-bytes";
 import { Context } from "../util";
 
 export const open = true;
 export const name = "bytes";
 export default async function ({ message, args, api }: Context) {
-  const prettyBytes = (await prettyBytesPkg).default;
   let bytes = parseInt(args[0]);
   if (!bytes) return await api.createMessage(message.channel_id, { content: "invalid amount of bytes" });
 
