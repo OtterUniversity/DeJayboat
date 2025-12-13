a4e9e204937a782b941db87580c488d9c5bb1165
Techno <68407783+Commandtechno@users.noreply.github.com>
2025-12-13 16:41:20 -0700

    Adjust description formatting based on output type

--- src/commands/admin/eval.ts
+++ src/commands/admin/eval.ts
@@ -33,7 +33,7 @@ export default async function ({ message, args, api, ws }: Context) {
     if (res.length > max - prefix.length - suffix.length - slice.length)
       res = res.slice(0, max - prefix.length - suffix.length - slice.length) + "...";
 
-    const description = prefix + res + suffix;
+    const description = out === true ? res : prefix + res + suffix;
     api.createMessage(message.channel_id, { embeds: [{ color, description }] });
   } catch (e) {
     api.createMessage(message.channel_id, {
