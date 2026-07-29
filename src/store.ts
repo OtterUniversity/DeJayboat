cf0c55327c229b35befcb1962b65ee5536cec95d
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2026-07-29 12:21:42 -0700

    move store

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,11 @@
 import { readFileSync, writeFileSync, existsSync, mkdirSync } from "fs";
 
-if (!existsSync("store")) mkdirSync("store");
-if (!existsSync("store/guilds.json")) writeFileSync("store/guilds.json", JSON.stringify({}));
-if (!existsSync("store/articles.json")) writeFileSync("store/articles.json", JSON.stringify([]));
-if (!existsSync("store/shutdown.json")) writeFileSync("store/shutdown.json", JSON.stringify({}));
-if (!existsSync("store/birthdays.json"))
-  writeFileSync("store/birthdays.json", JSON.stringify({ users: {}, lastAnnounced: null }));
+if (!existsSync("../store")) mkdirSync("../store");
+if (!existsSync("../store/guilds.json")) writeFileSync("../store/guilds.json", JSON.stringify({}));
+if (!existsSync("../store/articles.json")) writeFileSync("../store/articles.json", JSON.stringify([]));
+if (!existsSync("../store/shutdown.json")) writeFileSync("../store/shutdown.json", JSON.stringify({}));
+if (!existsSync("../store/birthdays.json"))
+  writeFileSync("../store/birthdays.json", JSON.stringify({ users: {}, lastAnnounced: null }));
 
 type Guilds = Record<string, string>;
 type Articles = number[];
@@ -25,23 +25,23 @@ interface Birthdays {
   lastAnnounced: string;
 }
 
-export const guilds: Guilds = JSON.parse(readFileSync("store/guilds.json", "utf-8"));
-export const articles: Articles = JSON.parse(readFileSync("store/articles.json", "utf-8"));
-export const shutdown: Shutdown = JSON.parse(readFileSync("store/shutdown.json", "utf-8"));
-export const birthdays: Birthdays = JSON.parse(readFileSync("store/birthdays.json", "utf-8"));
+export const guilds: Guilds = JSON.parse(readFileSync("../store/guilds.json", "utf-8"));
+export const articles: Articles = JSON.parse(readFileSync("../store/articles.json", "utf-8"));
+export const shutdown: Shutdown = JSON.parse(readFileSync("../store/shutdown.json", "utf-8"));
+export const birthdays: Birthdays = JSON.parse(readFileSync("../store/birthdays.json", "utf-8"));
 
 export function updateGuilds() {
-  writeFileSync("store/guilds.json", JSON.stringify(guilds));
+  writeFileSync("../store/guilds.json", JSON.stringify(guilds));
 }
 
 export function updateArticles() {
-  writeFileSync("store/articles.json", JSON.stringify(articles));
+  writeFileSync("../store/articles.json", JSON.stringify(articles));
 }
 
 export function updateShutdown(data: Shutdown) {
-  writeFileSync("store/shutdown.json", JSON.stringify(data));
+  writeFileSync("../store/shutdown.json", JSON.stringify(data));
 }
 
 export function updateBirthdays() {
-  writeFileSync("store/birthdays.json", JSON.stringify(birthdays));
+  writeFileSync("../store/birthdays.json", JSON.stringify(birthdays));
 }
